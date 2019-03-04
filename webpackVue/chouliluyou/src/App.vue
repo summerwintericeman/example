4480a369a665a84a52ba2b239d23e9d679e63d5d
wujiajia <979226469@qq.com>
2019-03-04 21:35:53 +0800

    119

---
+++ webpackVue/chouliluyou/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h1>这是App组件</h1>
+		
+		
+		<router-link to="/account">account</router-link>
+		<router-link to="/goodslist">goodslist</router-link>
+
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
--- webpackVueCode/src/login.vue
+++ webpackVueCode/src/login.vue
@@ -1,12 +1,28 @@
 <template>
 	<div>
 		这里是webpack使用vue的形式，登录组件
-		
+		--- {{msg}}
 	</div>
 </template>
 
 <script>
+	export default{
+		data(){
+			return{
+				msg: "123"
+			}
+		},
+		methods:{
+			show(){
+				console.log("login组件")
+			}
+		}
+	}
+	
 </script>
 
-<style>
+<style scoped="scoped">
+	div{
+		color: red;
+	}
 </style>
